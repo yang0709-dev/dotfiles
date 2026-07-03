4cd2acc23babfa6b2d2e868230ac3cccde5e0034
yang0709-dev <ertyu8571@gmail.com>
2026-07-03 15:46:30 +0800

    fixd

--- polybar/warp-status.sh
+++ polybar/warp-status.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if warp-cli status | grep -q "Connected"; then
-    echo "%{F#F0C674}Warp%{F-} %{F#FFFFFF}ON%{F-}"
+    echo "%{F#F0C674}Warp%{F-} %{F#C5C8C6}ON%{F-}"
 else
-    echo "%{F#F0C674}Warp%{F-} %{F#FFFFFF}OFF%{F-}"
+    echo "%{F#F0C674}Warp%{F-} %{F#C5C8C6}OFF%{F-}"
 fi
